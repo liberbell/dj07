22711a780e8f5223e203c72cc3da9b490f6c8485
Liberbell <liberbell@gmail.com>
2022-09-23 10:44:15 +0900

    models2

--- myblog/posts/models.py
+++ myblog/posts/models.py
@@ -5,3 +5,4 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     publish_date = models.DateTimeField()
     image = models.ImageField(upload_to="media/")
+    body = models.TextField(max_length=1024)
