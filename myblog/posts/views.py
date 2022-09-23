9d25ef31f7d68dd3fbeb351a935e0988399543b5
Liberbell <liberbell@gmail.com>
2022-09-23 11:08:27 +0900

    views2

--- myblog/posts/views.py
+++ myblog/posts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 def index(request):
     # return HttpResponse("Hello world. This is a index page.")
-    return render(request, "posts/index.html")
+    posts = Post.objects.order_by("-publish_date")
+    return render(request, "posts/index.html", {"posts": posts})
