18407ca2bb94fffe7257b0d7e667f9cbd8db4c2a
Liberbell <liberbell@gmail.com>
2022-09-24 10:52:31 +0900

    urls3

--- myblog/posts/urls.py
+++ myblog/posts/urls.py
@@ -1,7 +1,8 @@
 from . import views
 from django.urls import path
+from posts import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('posts/<int:post_id>[0-9]', views.)
+    path('posts/<int:post_id>[0-9]', views.post_detail, name="post_detail"),
 ]
