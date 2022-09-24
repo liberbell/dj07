db883ed9bb3778729247ad14ecc3bf5ce3abac59
Liberbell <liberbell@gmail.com>
2022-09-24 10:36:58 +0900

    urls2

--- myblog/myblog/urls.py
+++ myblog/myblog/urls.py
@@ -21,4 +21,4 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/', include('posts.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
