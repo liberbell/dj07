b5fa0b665c53744ac9ecff688df77410273b7235
Liberbell <liberbell@gmail.com>
2022-09-25 22:07:20 +0900

    views1

--- myblog/posts/views.py
+++ myblog/posts/views.py
@@ -7,3 +7,6 @@ def index(request):
     # return HttpResponse("Hello world. This is a index page.")
     posts = Post.objects.order_by("-publish_date")
     return render(request, "posts/index.html", {"posts": posts})
+
+def post_detail(request, post_id):
+    return render(request, "posts/post_detail.html", {"post_id": post_id})
