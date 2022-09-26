5e2c46b70ab342a36be730e4f2b9bbd72b2ac37d
Liberbell <liberbell@gmail.com>
2022-09-27 08:50:02 +0900

    lesson34

--- lesson3.py
+++ lesson3.py
@@ -5,11 +5,18 @@
 # except:
 #     print("An unknown error has occured.")
 
-try:
-    f = open("nonexits.txt", "r")
-except FileNotFoundError:
-    print("No such file.")
-except:
-    print("An unknown error has occured.")
+# try:
+#     f = open("nonexits.txt", "r")
+# except FileNotFoundError:
+#     print("No such file.")
+# except:
+#     print("An unknown error has occured.")
 
-input_var = int(input("Please enter a number:"))
+# input_var = int(input("Please enter a number:"))
+
+while True:
+    try:
+        input_var = int(input("Please enter a number:"))
+        break
+    except:
+        print("Ooops: That was not a valid number. Try again...")
