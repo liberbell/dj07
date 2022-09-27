ca0f3559aaf719f9fa6bd58b78d972bc26aaae84
Liberbell <liberbell@gmail.com>
2022-09-28 08:55:22 +0900

    lesson41

---
+++ lesson4.py
@@ -0,0 +1,11 @@
+num_list = [10, 20, 30, 40, 50]
+
+def main():
+    count = 0
+    total = 0
+
+    for i in num_list:
+        count += count + i
+        total += total + i
+
+        print("element is: ", i)
