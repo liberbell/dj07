b34ab6b5dab8e24b77554e670fc273a717df81a9
Liberbell <liberbell@gmail.com>
2022-09-29 08:46:21 +0900

    lesson53

--- lesson5.py
+++ lesson5.py
@@ -3,3 +3,4 @@ import sys
 argment_list = sys.argv
 print("The script has the name ", '"', (sys.argv[0]))
 print("Number of argments of ", (len(sys.argv)))
+print("The Arguments are ", argment_list)
