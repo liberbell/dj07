627264065aec6d7d331c940b1be97a3af9ea15a5
Liberbell <liberbell@gmail.com>
2022-09-29 08:52:45 +0900

    lesson54

--- lesson5.py
+++ lesson5.py
@@ -1,6 +1,13 @@
 import sys
+import argparse
 
-argment_list = sys.argv
-print("The script has the name ", '"', (sys.argv[0]))
-print("Number of argments of ", (len(sys.argv)))
-print("The Arguments are ", argment_list)
+# argment_list = sys.argv
+# print("The script has the name ", '"', (sys.argv[0]))
+# print("Number of argments of ", (len(sys.argv)))
+# print("The Arguments are ", argment_list)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-display")
+args = parser.parse_args()
+
+print(args.display)
