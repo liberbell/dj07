e5a201e9f6558c05653b6c785bccdae3f698b666
Liberbell <liberbell@gmail.com>
2022-09-29 09:06:26 +0900

    lessson63

--- lessson6.py
+++ lessson6.py
@@ -1,6 +1,11 @@
 import sys
+import argparse
 
-arg1 = int(sys.argv[1])
-arg2 = int(sys.argv[2])
+# arg1 = int(sys.argv[1])
+# arg2 = int(sys.argv[2])
 
-print("Addition of two numbers is ", arg1 + arg2)
+# print("Addition of two numbers is ", arg1 + arg2)
+
+ap = argparse.ArgumentParser()
+
+ap.add_argument("-a", "--first_operand", required=True, help="first operand")
