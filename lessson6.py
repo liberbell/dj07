ba07eceb50b785eaf933d69152276307590baec8
Liberbell <liberbell@gmail.com>
2022-09-30 08:28:37 +0900

    lessson61

--- lessson6.py
+++ lessson6.py
@@ -10,3 +10,8 @@ ap = argparse.ArgumentParser()
 
 ap.add_argument("-a", "--first_operand", required=True, help="first operand")
 ap.add_argument("-b", "--second_operand", required=True, help="second operand")
+
+args = vars(ap.parse_args())
+
+a = int(args["first operand"])
+b = int(args["second operand"])
