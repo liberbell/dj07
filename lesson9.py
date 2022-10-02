a294c4f6d640ef3a461f45447c2d1b6d5cb7ecf9
Liberbell <liberbell@gmail.com>
2022-10-03 08:45:23 +0900

    lesson92

--- lesson9.py
+++ lesson9.py
@@ -1,4 +1,5 @@
 for i in range(1, 10):
     if i % 2 == 0:
-        print("The number %s is devided by 2" % i)
+        print("The number %s is divisible by 2" % i)
     else:
+        print("The number %s is not divisible by 2" % i)
