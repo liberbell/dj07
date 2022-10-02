43695a8abf25a9ed2dca448717bd84f620f6ea56
Liberbell <liberbell@gmail.com>
2022-10-03 08:47:31 +0900

    lesson93

--- lesson9.py
+++ lesson9.py
@@ -3,3 +3,8 @@ for i in range(1, 10):
         print("The number %s is divisible by 2" % i)
     else:
         print("The number %s is not divisible by 2" % i)
+
+num_list = []
+for i in range(1, 21):
+    if (i % 3 == 0 or i % 5 == 0):
+        num_list.append(i)
