0cdb3180e18e5c345490b6cc749f062a41352007
Liberbell <liberbell@gmail.com>
2022-10-02 14:49:12 +0900

    lesson84

--- lesson8.py
+++ lesson8.py
@@ -1,7 +1,10 @@
-my_range = range(5)
-print(my_range)
-print(tuple(my_range))
-print(list(my_range))
-print(list(range(5, 10)))
+# my_range = range(5)
+# print(my_range)
+# print(tuple(my_range))
+# print(list(my_range))
+# print(list(range(5, 10)))
 
-print(list(range(-5, 5)))
+# print(list(range(-5, 5)))
+
+for i in range(3.3):
+    print(i)
