606194326c9db374d7f0a98ba0e73bad1069c531
Liberbell <liberbell@gmail.com>
2022-10-04 08:32:18 +0900

    lesson95

--- lesson9.py
+++ lesson9.py
@@ -27,6 +27,6 @@ for num1 in range(1, 10):
     print("num = ", num1, ":", end=" ")
 
     for num2 in range(1, 10):
-        print("{:2d}".format(num1 + num2), end=" " )
+        print("{:2d}".format(num1 * num2), end=" " )
 
     print()
