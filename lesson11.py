46b9f06bdfaa61da61c5def9a30a7c439cf9cdeb
Liberbell <liberbell@gmail.com>
2022-10-04 08:55:46 +0900

    lesson115

--- lesson11.py
+++ lesson11.py
@@ -9,9 +9,15 @@
 #     if letter == "o":
 #         break
 
-for char in "icecream":
-    if char == "c":
-        print("The letter %s was found in the string." % char)
-        break
-else:
-    print("The letter %s does not exist in the string." % char)
+# for char in "icecream":
+#     if char == "c":
+#         print("The letter %s was found in the string." % char)
+#         break
+# else:
+#     print("The letter %s does not exist in the string." % char)
+
+for num in [3, 11, 22, 35, 90]:
+    print(num)
+
+    if (num == 22):
+        print("The number %d has been found" % num)
