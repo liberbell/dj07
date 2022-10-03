1365f730a311feda532c3436a635e5026d0e97fa
Liberbell <liberbell@gmail.com>
2022-10-04 08:37:53 +0900

    lesson107

--- lesson10.py
+++ lesson10.py
@@ -1,7 +1,10 @@
-original_list = [1, 2, 3, 4, 5, 6]
-cubes_list = []
+# original_list = [1, 2, 3, 4, 5, 6]
+# cubes_list = []
 
-for i in original_list:
-    cubes_list.append(i ** 3)
+# for i in original_list:
+#     cubes_list.append(i ** 3)
 
-print(cubes_list)
+# print(cubes_list)
+
+original_list = [["Volkswagen", "Mercedes", "BMW"], ["Honda", "Toyota", "Mazda"]]
+flattened_lists = []
