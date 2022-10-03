af4c0853121598290030727369d4124d7cd309ca
Liberbell <liberbell@gmail.com>
2022-10-04 08:50:43 +0900

    lesson113

--- lesson11.py
+++ lesson11.py
@@ -1,9 +1,10 @@
-for letter in "Python":
-    if letter == "o":
-        break
-    print(letter)
+# for letter in "Python":
+#     if letter == "o":
+#         break
+#     print(letter)
 
 for letter in "Python":
     print(letter)
     
     if letter == "o":
+        break
