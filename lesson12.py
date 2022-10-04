d1d249acd28aa6798762cf49103923327f844a9f
Liberbell <liberbell@gmail.com>
2022-10-05 08:42:52 +0900

    lesson126

--- lesson12.py
+++ lesson12.py
@@ -8,10 +8,18 @@
 # else:
 #     print("Teh villain got away.")
 
-num = int(input("Enter a number: "))
-for i in range(2, num//2):
-    if num % i == 0:
-        print("It is not a prime number.")
+# num = int(input("Enter a number: "))
+# for i in range(2, num//2):
+#     if num % i == 0:
+#         print("It is not a prime number.")
+#         break
+# else:
+#     print(num, "is a prime number.")
+
+for i in range(3):
+    password = str(input("Enter a password"))
+    if password == "secret":
+        print("You guessed the password :)")
         break
 else:
-    print(num, "is a prime number.")
+    print("3 incorrect password attempt.")
