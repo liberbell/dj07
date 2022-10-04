5aa8e79688eda89d37673ccdefef3c6fb9f6cac4
Liberbell <liberbell@gmail.com>
2022-10-05 08:32:34 +0900

    lesson122

--- lesson12.py
+++ lesson12.py
@@ -1,1 +1,6 @@
 places = ["New Zealand", "Norway", "Botswana", "Zimbabwe", "Uzbekistan", "Paraguay"]
+villain_at = "Mali"
+
+for place in places:
+    if place == villain_at:
+        print("The villain has been captured!")
