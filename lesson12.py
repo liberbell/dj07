cd644c2d354eb7564644723c4267c32da9c76a50
Liberbell <liberbell@gmail.com>
2022-10-05 08:36:34 +0900

    lesson124

--- lesson12.py
+++ lesson12.py
@@ -1,9 +1,13 @@
-places = ["New Zealand", "Norway", "Botswana", "Zimbabwe", "Uzbekistan", "Paraguay"]
-villain_at = "Mali"
+# places = ["New Zealand", "Norway", "Botswana", "Zimbabwe", "Uzbekistan", "Paraguay"]
+# villain_at = "Norway"
 
-for place in places:
-    if place == villain_at:
-        print("The villain has been captured!")
-        break
-else:
-    print("Teh villain got away.")
+# for place in places:
+#     if place == villain_at:
+#         print("The villain has been captured!")
+#         break
+# else:
+#     print("Teh villain got away.")
+
+num = int(input("Enter a number: "))
+for i in range(2, num//2):
+    if num % i == 0:
