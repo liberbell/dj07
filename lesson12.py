bd9a4f2934489688df9c979c0fc7bb6825b772f5
Liberbell <liberbell@gmail.com>
2022-10-05 08:37:35 +0900

    lesson126

--- lesson12.py
+++ lesson12.py
@@ -12,5 +12,6 @@ num = int(input("Enter a number: "))
 for i in range(2, num//2):
     if num % i == 0:
         print("It is not a prime number.")
+        break
 else:
     print(num, "is a prime number.")
