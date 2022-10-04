07cc25d514505f6d6c5a160b726a925172a6ad2a
Liberbell <liberbell@gmail.com>
2022-10-05 08:33:05 +0900

    lesson123

--- lesson12.py
+++ lesson12.py
@@ -4,3 +4,6 @@ villain_at = "Mali"
 for place in places:
     if place == villain_at:
         print("The villain has been captured!")
+        break
+    else:
+        print("Teh villain got away.")
