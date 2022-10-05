cc16b7a4d69c61ffe840f41c08d85290fd7a4891
Liberbell <liberbell@gmail.com>
2022-10-06 08:50:05 +0900

    lesson1314

--- lesson13.py
+++ lesson13.py
@@ -37,3 +37,6 @@
 num_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 new_dict = dict()
+
+for i, j in zip(months, num_days):
+    new_dict.update({i:j})
