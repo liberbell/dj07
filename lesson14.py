a0ae39df8b0af04cd008c712fb478d730489f6bf
Liberbell <liberbell@gmail.com>
2022-10-07 08:37:16 +0900

    lesson145

--- lesson14.py
+++ lesson14.py
@@ -6,8 +6,12 @@
 #         print("Current Letter: ", letter)
     # print(letter)
 
+# for i in range(10):
+#     if(i % 2 == 0):
+#         pass
+#     else:
+#         print(i)
+
+odd_count = 0
+total_count = 0
 for i in range(10):
-    if(i % 2 == 0):
-        pass
-    else:
-        print(i)
