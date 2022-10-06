945126162e058dc4c081cb67082e47c13d487fbd
Liberbell <liberbell@gmail.com>
2022-10-07 08:38:09 +0900

    lesson146

--- lesson14.py
+++ lesson14.py
@@ -15,3 +15,12 @@
 odd_count = 0
 total_count = 0
 for i in range(10):
+    if(i % 2 == 0):
+        pass
+    else:
+        print(i)
+        odd_count += 1
+
+    total_count += 1
+
+    print("Count")
