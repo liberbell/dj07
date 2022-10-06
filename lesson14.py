f3eee76ed0f7629da07ebefd630612287389af7b
Liberbell <liberbell@gmail.com>
2022-10-07 08:35:33 +0900

    lesson146

--- lesson14.py
+++ lesson14.py
@@ -1,7 +1,13 @@
-for letter in "string":
-    if letter == "r":
-        pass
-        print("This is the pass block")
-    else:
-        print("Current Letter: ", letter)
+# for letter in "string":
+#     if letter == "r":
+#         pass
+#         print("This is the pass block")
+#     else:
+#         print("Current Letter: ", letter)
     # print(letter)
+
+for i in range(10):
+    if(i % 2 == 0):
+        pass
+    else:
+        print(i)
