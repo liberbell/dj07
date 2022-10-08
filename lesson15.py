174ca8ab8ff758d96cf98eaace8815c93dce4da0
Liberbell <liberbell@gmail.com>
2022-10-08 10:37:06 +0900

    lesson153

--- lesson15.py
+++ lesson15.py
@@ -1,5 +1,7 @@
 # num = range(10)
 
 # print([x * x for x in num])
-letters = [letter for letter in "anxiety"]
-print(letters)
+# letters = [letter for letter in "anxiety"]
+# print(letters)
+
+list_of_words = ["this", "is", "a", "list", "of", "words"]
