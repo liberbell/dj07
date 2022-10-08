a25ef254d07d8b5f88c410dba52d5ae4243fb227
Liberbell <liberbell@gmail.com>
2022-10-08 10:39:34 +0900

    lesson155

--- lesson15.py
+++ lesson15.py
@@ -4,6 +4,10 @@
 # letters = [letter for letter in "anxiety"]
 # print(letters)
 
+# list_of_words = ["this", "is", "a", "list", "of", "words"]
+# items = [word[0] for word in list_of_words]
+# print(items)
+
 list_of_words = ["this", "is", "a", "list", "of", "words"]
-items = [word[0] for word in list_of_words]
+items = [word[0].upper() for word in list_of_words]
 print(items)
