29a1a952929d43261f4cb58fe0ed3e1d060170cb
Liberbell <liberbell@gmail.com>
2022-10-08 10:41:45 +0900

    lesson157

--- lesson15.py
+++ lesson15.py
@@ -1,4 +1,4 @@
-# num = range(10)
+num = range(10)
 
 # print([x * x for x in num])
 # letters = [letter for letter in "anxiety"]
@@ -8,9 +8,11 @@
 # items = [word[0] for word in list_of_words]
 # print(items)
 
-list_of_words = ["this", "is", "a", "list", "of", "words"]
-items = [word[0].upper() for word in list_of_words]
-print(items)
+# list_of_words = ["this", "is", "a", "list", "of", "words"]
+# items = [word[0].upper() for word in list_of_words]
+# print(items)
 
-items = [word.upper() for word in list_of_words]
-print(items)
+# items = [word.upper() for word in list_of_words]
+# print(items)
+
+print([x * x for x in num if x % 2 != 0])
