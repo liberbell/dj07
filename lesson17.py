e19d9be3a6a10e67cad5b0d0d8c35cee9a1f27e8
Liberbell <liberbell@gmail.com>
2022-10-09 15:24:18 +0900

    lesson174

--- lesson17.py
+++ lesson17.py
@@ -2,6 +2,11 @@ import numbers
 
 
 number = 20
-while number < 25:
+# while number < 25:
+#     print(number)
+#     number += 1
+
+while number <= (25 + 5):
     print(number)
+    
     number += 1
