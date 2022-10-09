9214b34ba1e2c66f0187e2190aef60a751aa9b50
Liberbell <liberbell@gmail.com>
2022-10-09 15:02:51 +0900

    lesson164

--- lesson16.py
+++ lesson16.py
@@ -1,7 +1,9 @@
-lists_of_num = [124, 148, 173, 231, 256, 301, 351, 365, 399]
-for i in lists_of_num:
-    if(i == 351):
-        break
-    if(i % 7 == 0):
-        continue
-    print(i, end=" ")
+# lists_of_num = [124, 148, 173, 231, 256, 301, 351, 365, 399]
+# for i in lists_of_num:
+#     if(i == 351):
+#         break
+#     if(i % 7 == 0):
+#         continue
+#     print(i, end=" ")
+
+original_list = [2, 3, 6, 7, 8, 11, 12, 13, 17, 18]
