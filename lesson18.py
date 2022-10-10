491345decb91926a9b192fcfc91d221ed5ddb75c
Liberbell <liberbell@gmail.com>
2022-10-11 08:37:50 +0900

    lesson184

--- lesson18.py
+++ lesson18.py
@@ -1,6 +1,14 @@
+from operator import le
+
+
 num_list = [20, 30, 40, 50, 60]
 count = 0
 
+# while count < len(num_list):
+#     print(num_list[count])
+#     count = count + 1
+
 while count < len(num_list):
-    print(num_list[count])
     count = count + 1
+
+    print("There are %d elements in the list" % count)
