b8d8aecac1b5fc1e6849558b10efe8a917c6e78e
Liberbell <liberbell@gmail.com>
2022-10-11 08:30:50 +0900

    lesson171

--- lesson17.py
+++ lesson17.py
@@ -53,10 +53,13 @@ from operator import le
 
 #     num += 1
 
-value_int = int(input("Enter an integer: "))
-length = 0
+# value_int = int(input("Enter an integer: "))
+# length = 0
 
-while value_int > 0:
-    value_int //= 10
-    length += 1
-print("number of digits: ", length)
+# while value_int > 0:
+#     value_int //= 10
+#     length += 1
+# print("number of digits: ", length)
+
+x, y = 0, 8
+while(x < y): x = x + y; print(x, y)
