09d10060890cb33db1c7e627bff7f045f8027ea7
Liberbell <liberbell@gmail.com>
2022-10-13 08:39:50 +0900

    lesson196

--- lesson19.py
+++ lesson19.py
@@ -4,11 +4,17 @@
 #     print(number)
 #     break
 num = 10
-while num in range(10, 100):
-    print(num)
+# while num in range(10, 100):
+#     print(num)
 
-    if num == 50:
+#     if num == 50:
+#         break
+
+#     num += 10
+# print("out of the loop")
+while True:
+    num = int(input("Enter a number: "))
+    if num % 3 == 0:
         break
 
-    num += 10
-print("out of the loop")
+print("\nWe are out of the loop since we encoounterd the number:", num)
