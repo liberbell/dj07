b28f3d8c260905f2081ac2525069899ca1472d93
Liberbell <liberbell@gmail.com>
2022-10-15 14:15:35 +0900

    lesson212

--- lesson21.py
+++ lesson21.py
@@ -1,3 +1,6 @@
 response = ""
 while True:
     response = input("\nWhat is the capital of United Kingdom.")
+
+    if response == "quit":
+        print("The correct answer is London. Better luck next time.")
