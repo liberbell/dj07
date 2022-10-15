434ebd95469c0b3c3f53509655a0f40dd276689b
Liberbell <liberbell@gmail.com>
2022-10-15 14:04:37 +0900

    lesson202

--- lesson20.py
+++ lesson20.py
@@ -8,13 +8,15 @@
 #     print("odd number : ", num)
 # print("bye!")
 
-my_list = []
-num = 0
+# my_list = []
+# num = 0
 
-while num in range(0, 50):
-    num += 1
-    if num % 5 == 0 or num % 7 == 0:
-        continue
-    my_list.append(num)
+# while num in range(0, 50):
+#     num += 1
+#     if num % 5 == 0 or num % 7 == 0:
+#         continue
+#     my_list.append(num)
 
-print(my_list)
+# print(my_list)
+while True:
+    value = input("\nEnter a number: ")
