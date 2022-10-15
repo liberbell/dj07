eb61efd23b301abfc28222388d2e1a19c1c98c78
Liberbell <liberbell@gmail.com>
2022-10-15 14:56:34 +0900

    lesson225

--- lesson22.py
+++ lesson22.py
@@ -1,3 +1,6 @@
 import dash
 from dash import dcc
 from dash import html
+
+external_stylesheet = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+app = dash.Dash(__name__, external_stylesheets=external_stylesheet)
