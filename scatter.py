41ca45520651cff7f4e872fdb351bbed3d85e91a
Liberbell <liberbell@gmail.com>
2022-10-17 08:37:55 +0900

    scatter1

---
+++ scatter.py
@@ -0,0 +1,12 @@
+import dash
+from dash import dcc
+from dash import html
+
+external_stylesheet = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheet)
+
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
