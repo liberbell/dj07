3005d8e68ac392d9d5e9b8d93c2881192821c356
Liberbell <liberbell@gmail.com>
2022-10-16 13:51:28 +0900

    lesson222

--- lesson22.py
+++ lesson22.py
@@ -13,5 +13,10 @@ app.layout = html.Div(children =[
 
     dcc.Graph(
         id = "example-graph",
+        figure = {
+            "data" : [
+                {"x": [1, 2, 3], "y": [4, 1, 2]}
+            ]
+        }
     )
 ])
