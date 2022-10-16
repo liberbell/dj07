297b13cb19d91f1cd812b641508c878a81c66628
Liberbell <liberbell@gmail.com>
2022-10-16 13:49:32 +0900

    lesson221

--- lesson22.py
+++ lesson22.py
@@ -4,3 +4,14 @@ from dash import html
 
 external_stylesheet = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 app = dash.Dash(__name__, external_stylesheets=external_stylesheet)
+
+app.layout = html.Div(children =[
+    html.H1(children="Hello Dash"),
+    html.Div(children = '''
+    Dash: A web application framework for python.
+    '''),
+
+    dcc.Graph(
+        id = "example-graph",
+    )
+])
