3ab3ef0b9564a604302f27b3b70e209a7ff775e2
Liberbell <liberbell@gmail.com>
2022-10-18 08:22:42 +0900

    line2

--- line.py
+++ line.py
@@ -1,12 +1,15 @@
 import dash
 from dash import dcc
 from dash import html
+import pandas as pd
+
 
 external_stylesheet = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 app = dash.Dash(__name__, external_stylesheets=external_stylesheet)
 
+df=pd.read_csv("time_sereis.csv")
 
-
+print(df.head())
 
 if __name__ == "__main__":
     app.run_server(debug=True)
