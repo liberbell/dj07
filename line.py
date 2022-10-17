bc28ac15d14b9843cb141459f78bca6f2193e900
Liberbell <liberbell@gmail.com>
2022-10-18 08:23:34 +0900

    line3

--- line.py
+++ line.py
@@ -7,9 +7,10 @@ import pandas as pd
 external_stylesheet = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 app = dash.Dash(__name__, external_stylesheets=external_stylesheet)
 
-df=pd.read_csv("time_sereis.csv")
+df=pd.read_csv("time_series.csv")
 
 print(df.head())
+print(df["MSFT"])
 
-if __name__ == "__main__":
-    app.run_server(debug=True)
+# if __name__ == "__main__":
+#     app.run_server(debug=True)
