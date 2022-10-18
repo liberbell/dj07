27f4d1bcdeb95e3d6233e8e0125a861cc82102eb
Liberbell <liberbell@gmail.com>
2022-10-19 08:57:16 +0900

    markdown15

--- dash/markdown.py
+++ dash/markdown.py
@@ -53,7 +53,8 @@ app.layout = html.Div([
         min=0,
         max=10,
         marks={i:i for i in range(1, 6)},
-        value=3
+        value=3,
+        id="Slider1"
     )
 #     dcc.Markdown('''
 # # Title 1
