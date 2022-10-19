1a60fbb56bb76cd492fb61c3d2c13838c33b736d
Liberbell <liberbell@gmail.com>
2022-10-20 08:52:11 +0900

    collectdata4

--- app/collectdata.py
+++ app/collectdata.py
@@ -2,6 +2,7 @@ from operator import imod
 from bs4 import BeautifulSoup
 import requests
 
-URL = "https://scraping-for-begginer.herokuapp.com/udemy"
+URL = "https://scraping-for-beginner.herokuapp.com/udemy"
 
 data1 = requests.get(URL)
+print(data1.status_code)
