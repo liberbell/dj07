d28aa6a0a69b54eb71ff386d42f3ab425529063a
Liberbell <liberbell@gmail.com>
2022-10-20 08:58:10 +0900

    collectdata5

--- app/collectdata.py
+++ app/collectdata.py
@@ -5,8 +5,9 @@ import requests
 URL = "https://scraping-for-beginner.herokuapp.com/udemy"
 
 data1 = requests.get(URL)
-print(data1.status_code)
-print(data1.text)
+# print(data1.status_code)
+# print(data1.text)
 
 soup = BeautifulSoup(data1.text, "html.parser")
-print(soup)
+# print(soup)
+print(soup.select(".card-title")[0].string)
