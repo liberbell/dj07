28e6af207ab8500ec42017953577e93ff19f7cb6
Liberbell <liberbell@gmail.com>
2022-10-20 09:04:40 +0900

    collectdata7

--- app/collectdata.py
+++ app/collectdata.py
@@ -21,3 +21,5 @@ print(students_split[1])
 
 reviewer = soup.select("body > div.row > div > div:nth-child(2) > div > div > div.card-action > p.reviews")[0].string
 print(reviewer)
+reviewer_split = reviewer.split("：")
+print(reviewer_split[1])
