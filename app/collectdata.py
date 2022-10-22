06754b8d49b65210ef8de50bc41ffa76696cc3a2
Liberbell <liberbell@gmail.com>
2022-10-22 14:19:04 +0900

    collectdata9

--- app/collectdata.py
+++ app/collectdata.py
@@ -4,34 +4,34 @@ import requests
 import pandas as pd
 import datetime
 
-URL = "https://scraping-for-beginner.herokuapp.com/udemy"
+# URL = "https://scraping-for-beginner.herokuapp.com/udemy"
 
-data1 = requests.get(URL)
+# data1 = requests.get(URL)
 # print(data1.status_code)
 # print(data1.text)
 
-soup = BeautifulSoup(data1.text, "html.parser")
+# soup = BeautifulSoup(data1.text, "html.parser")
 # print(soup)
-name= soup.select(".card-title")[0].string
+# name= soup.select(".card-title")[0].string
 # print(name)
 
-students = soup.select("body > div.row > div > div:nth-child(2) > div > div > div.card-action > p.subscribers")[0].string
+# students = soup.select("body > div.row > div > div:nth-child(2) > div > div > div.card-action > p.subscribers")[0].string
 # print(students)
-students_split = students.split("：")
-students_num = int(students_split[1])
+# students_split = students.split("：")
+# students_num = int(students_split[1])
 # print(students_num)
 
-reviewer = soup.select("body > div.row > div > div:nth-child(2) > div > div > div.card-action > p.reviews")[0].string
+# reviewer = soup.select("body > div.row > div > div:nth-child(2) > div > div > div.card-action > p.reviews")[0].string
 # print(reviewer)
-reviewer_split = reviewer.split("：")
-reviewer_num = int(reviewer_split[1])
+# reviewer_split = reviewer.split("：")
+# reviewer_num = int(reviewer_split[1])
 # print(reviewer_num)
 
-results ={
-    "name": name,
-    "students": students_num,
-    "reviewers": reviewer_num,
-}
+# results ={
+#     "name": name,
+#     "students": students_num,
+#     "reviewers": reviewer_num,
+# }
 # print(results)
 
 df = pd.read_csv("assets/studentsnum.csv")
@@ -63,3 +63,9 @@ def get_udemy():
     reviewer = soup.select(".reviews")[0].string
     reviewer_split = reviewer.split("：")
     reviewer_num = int(reviewer_split[1])
+
+    results ={
+    "name": name,
+    "students": students_num,
+    "reviewers": reviewer_num,
+    }
