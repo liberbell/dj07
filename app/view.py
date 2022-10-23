2e72e29ef4c3fe37879cf5d88b76fc8b73a4d246
Liberbell <liberbell@gmail.com>
2022-10-23 14:28:19 +0900

    view2

--- app/view.py
+++ app/view.py
@@ -3,9 +3,22 @@ from dash import dcc
 from dash import html
 import plotly.graph_objs as go
 import pandas as pd
+import datetime
 
 
 external_stylesheet = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+df = pd.read_csv("assets/data.csv")
+dates = []
+for _date in df["date"]:
+    date = datetime.datetime.strptime(df["date"][0], "%Y/%m/%d").date()
+    dates.append(date)
+
+subscriver_num = df["subscribers"].values
+review_num = df["reviews"].values
+
+diff_subscribers = df["subscribers"].diff().values
+diff_reviews = df["reviews"].diff().values
+
 app = dash.Dash(__name__, external_stylesheets=external_stylesheet)
 
 
