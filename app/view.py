afe58bfdbf12a202e80fcc98e8e9eff3369ddac9
Liberbell <liberbell@gmail.com>
2022-10-24 08:51:22 +0900

    view15

--- app/view.py
+++ app/view.py
@@ -53,6 +53,7 @@ app.layout = html.Div(children=[
                     title="Subscribers Diff",
                     xaxis=dict(title="Date"),
                     yaxis=dict(title="Subscribers Num", side="left", showgrid=False, range=[2000, max(subscriver_num)+100]),
+                    yaxis2=dict(title="Subscribers Diff", side="right", showgrid=False, range=[0, max(diff_subscribers)+10]),
                 )
             }
         )
