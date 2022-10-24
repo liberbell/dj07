9819cd6cdf1cf696148f31840f20f205fd437469
Liberbell <liberbell@gmail.com>
2022-10-25 08:54:30 +0900

    database2

--- app/assets/database.py
+++ app/assets/database.py
@@ -1,2 +1,7 @@
+import imp
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+import datetime
+import os
