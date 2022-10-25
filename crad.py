7fe85e655522822e301ac2f6b09caf6ab1127acd
Liberbell <liberbell@gmail.com>
2022-10-26 08:51:32 +0900

    crad2

--- crad.py
+++ crad.py
@@ -1,8 +1,12 @@
 from dataclasses import dataclass
+from urllib.request import DataHandler
 from app.assets.database import db_session
 from app.assets.database import init_db
 from app.assets.models import Data
 
 import datetime
 
-init_db()
+# init_db()
+date = datetime.date.today()
+print(date)
+row = Data(date=DataHandler, subscribers=3500, reviews=200)
