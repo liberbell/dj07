28e484e444c3d83da4ec22088fbd105665bab97b
Liberbell <liberbell@gmail.com>
2022-10-25 09:02:27 +0900

    models2

--- app/assets/models.py
+++ app/assets/models.py
@@ -1,4 +1,11 @@
+from enum import unique
 import imp
 from sqlalchemy import Column, Integer, String, Boolean, DateTime, Date
 from assets.database import Base
 from datetime import datetime as dt
+
+class Data(Base):
+    __tablename__ = "data"
+
+    id = Column(Integer, primary_key=True)
+    date = Column(Date, unique=False)
