4607c91e23585e7f54d07bbc0573596491d64ccb
Liberbell <liberbell@gmail.com>
2022-10-28 08:52:19 +0900

    changedata3

--- app/assets/changedata.py
+++ app/assets/changedata.py
@@ -1,7 +1,12 @@
 import models
 import pandas as pd
+import datetime
 
 from database import db_session
 
 df = pd.read_csv("data.csv")
 print(df.head())
+
+print(type(df.iloc[0, 0]))
+fdate = datetime.datetime.strptime(df.iloc[0, 0], "%Y/%m/%d").date
+print(fdate, type(fdate))
