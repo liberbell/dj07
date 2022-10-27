1d432adeaccf419e148adb97b5a4658285e2ac3f
Liberbell <liberbell@gmail.com>
2022-10-28 08:59:00 +0900

    changedata7

--- app/assets/changedata.py
+++ app/assets/changedata.py
@@ -15,3 +15,6 @@ for index, _df in df.iterrows():
     fdate = datetime.datetime.strptime(_df["date"], "%Y/%m/%d").date()
     print(_df["date"])
     row = models.Data(date=date, subscribers=_df["subscribers"], reviews=_df["reviews"])
+    db_session.add(row)
+    
+db_session.commit()
