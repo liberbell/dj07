d459088dc6ba1681d1b18f4179368b5ca1b2ddfa
Liberbell <liberbell@gmail.com>
2022-10-28 08:45:37 +0900

    changedata1

--- app/assets/changedata.py
+++ app/assets/changedata.py
@@ -1,7 +1,7 @@
-from assets import models
+import models
 import pandas as pd
 
-from assets.database import db_session
+from database import db_session
 
 df = pd.read_csv("assets/data.csv")
 print(df.head())
