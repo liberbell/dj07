3a23f9aec06765d88d78250aea5dfe4ead295898
Liberbell <liberbell@gmail.com>
2022-10-28 08:53:02 +0900

    changedata4

--- app/assets/changedata.py
+++ app/assets/changedata.py
@@ -8,5 +8,5 @@ df = pd.read_csv("data.csv")
 print(df.head())
 
 print(type(df.iloc[0, 0]))
-fdate = datetime.datetime.strptime(df.iloc[0, 0], "%Y/%m/%d").date
+fdate = datetime.datetime.strptime(df.iloc[0, 0], "%Y/%m/%d").date()
 print(fdate, type(fdate))
