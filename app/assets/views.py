e77e00fd53a8d9b1d6627afbd4837184497ec125
Liberbell <liberbell@gmail.com>
2022-10-28 09:10:52 +0900

    views4

--- app/assets/views.py
+++ app/assets/views.py
@@ -2,4 +2,5 @@ from database import db_session
 from models import Data
 import pandas as pd
 
-data = db_session.query(Data.date, Data.subscribers, Data.reviews)
+data = db_session.query(Data.date, Data.subscribers, Data.reviews).all()
+print(data)
