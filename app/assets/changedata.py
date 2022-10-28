be6bda57266d31daffc74285c878966a70810179
Liberbell <liberbell@gmail.com>
2022-10-28 09:00:57 +0900

    changedata7

--- app/assets/changedata.py
+++ app/assets/changedata.py
@@ -11,10 +11,12 @@ df = pd.read_csv("data.csv")
 fdate = datetime.datetime.strptime(df.iloc[0, 0], "%Y/%m/%d").date()
 # print(fdate, type(fdate))
 
-for index, _df in df.iterrows():
-    fdate = datetime.datetime.strptime(_df["date"], "%Y/%m/%d").date()
-    print(_df["date"])
-    row = models.Data(date=date, subscribers=_df["subscribers"], reviews=_df["reviews"])
-    db_session.add(row)
-    
-db_session.commit()
+def read_data():
+    import models
+    for index, _df in df.iterrows():
+        fdate = datetime.datetime.strptime(_df["date"], "%Y/%m/%d").date()
+        print(_df["date"])
+        row = models.Data(date=date, subscribers=_df["subscribers"], reviews=_df["reviews"])
+        db_session.add(row)
+        
+    db_session.commit()
